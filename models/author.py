c0a6d62f4ad5b29f21a16a7e742b9e5032204e3d
Githinji Mugambi <ggathungu20905@gmail.com>
2024-06-10 16:06:58 +0300

    personal commit

--- models/author.py
+++ models/author.py
@@ -1,7 +1,94 @@
+from database.connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        assert len(name)>=0
 
     def __repr__(self):
         return f'<Author {self.name}>'
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name: str):
+       
+        if hasattr(self, '_name'):
+            print("Name cannot be changed after author is instantiated")
+            return 
+        self._name = name
+    name = property(get_name, set_name)
+    
+    def get_id(self):
+        return self._id
+    
+    def set_id(self, id):
+          if type(id) != int:
+            print("ID must be of type int")
+            return  
+          self._id = id
+
+    id = property(get_id, set_id)
+
+    @classmethod
+    def create(cls, name):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            INSERT INTO authors (name) VALUES (?)
+        ''', (name,))
+        
+        conn.commit()
+        authors_id = cursor.lastrowid
+        conn.close()
+        
+        return cls(authors_id, name)
+
+    @classmethod
+    def get_by_id(cls, author_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            SELECT id, name FROM authors WHERE id = ?
+        ''', (author_id,))
+        
+        row = cursor.fetchone()
+        conn.close()
+        
+        if row:
+            return cls(row['id'], row['name'])
+        else:
+            return None
+    def articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            SELECT articles.id, articles.title, articles.content, articles.magazine_id
+            FROM articles
+            INNER JOIN authors ON articles.author_id = authors.id
+            WHERE authors.id = ?
+        ''', (self.id,))
+        
+        articles = cursor.fetchall()
+        conn.close()
+
+        return articles
+    def magazines(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute('''
+        SELECT magazines.id, magazines.name, magazines.category
+        FROM magazines
+        INNER JOIN articles ON articles.magazine_id = magazines.id
+        WHERE articles.author_id = ?
+    ''', (self.id,))
+        
+        magazines = cursor.fetchall()
+        conn.close()
+
+        return magazines
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,134 @@
+from database.connection import get_db_connection
+from models.author import Author
+
 class Magazine:
     def __init__(self, id, name, category):
         self.id = id
         self.name = name
         self.category = category
+        assert len(name)>=2 and len(name)<=16
+        assert len(category)>=2 and len(category)<=16
 
     def __repr__(self):
         return f'<Magazine {self.name}>'
+    
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name: str):
+       
+    
+     name = property(get_name, set_name)
+    
+    def get_category(self):
+        return self._category
+    
+    def set_category(self, category:str):
+     
+        self._category = category
+
+    category = property(get_category, set_category)
+    
+    def get_id(self):
+        return self._id
+    
+    def set_id(self, id):
+        if type(id) != int:
+            return  
+        self._id = id
+
+    id = property(get_id, set_id)
+    
+    @classmethod
+    def create(cls, name, category):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            INSERT INTO magazines (name, category) VALUES (?, ?)
+        ''', (name, category))
+        
+        conn.commit()
+        magazines_id = cursor.lastrowid
+        conn.close()
+        
+        return cls(magazines_id, name, category)
+
+    @classmethod
+    def get_by_id(cls, magazine_id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            SELECT id, name, category FROM magazines WHERE id = ?
+        ''', (magazine_id,))
+        
+        fetch = cursor.fetchone()
+        conn.close()
+        
+        if fetch:
+            return cls(fetch['id'], fetch['name'], fetch['category'])
+        else:
+            return None
+        
+    def articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT articles.id, articles.title, articles.content, articles.author_id
+            FROM articles
+            WHERE articles.magazine_id = ?
+        ''', (self.id,))
+
+        articles = cursor.fetchall()
+        conn.close()
+
+        return articles
+    
+    def contributors(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()   
+        cursor.execute('''
+            SELECT authors.id, authors.name
+            FROM authors
+            INNER JOIN articles ON articles.author_id = authors.id
+            WHERE articles.magazine_id = ?
+        ''', (self.id,))
+        
+        contributors_rows = cursor.fetchall()
+        conn.close()
+        
+
+        return contributors_rows
+    def article_titles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT title FROM articles WHERE magazine_id = ?
+        ''', (self.id,))
+        titles = [row[0] for row in cursor.fetchall()]
+        conn.close()
+        if not titles:
+           return None
+        else:
+            return titles
+        
+    def contributing_authors(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('''
+            SELECT authors.id, authors.name
+            FROM articles
+            INNER JOIN authors ON articles.author_id = authors.id
+            WHERE articles.magazine_id = ?
+            GROUP BY authors.id
+            HAVING COUNT(*) > 2
+        ''', (self.id,))
+        authors_rows = cursor.fetchall()
+        conn.close()
+
+        if not authors_rows:
+            return None
+
+        contributing_authors = [Author(row['id'], row['name']) for row in authors_rows if type(row) is Author]
+        return contributing_authors
